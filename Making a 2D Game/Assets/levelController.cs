fadb04d24e50f916826ea37988a975ed28f7be21
CraigMcCorrisken <craigmccorrisken@gmail.com>
2020-07-19 14:24:30 +0100

    0.0.2 - Finishing tutorial
    Signed-off-by: CraigMcCorrisken <craigmccorrisken@gmail.com>

---
+++ Making a 2D Game/Assets/levelController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelController : MonoBehaviour
+{
+    private Enemy[] enemies;
+    private static int nextLevelIndex = 1;
+
+    private void OnEnable()
+    {
+        enemies = FindObjectsOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Enemy enemy in enemies)
+        {
+            if (enemy != null)
+                return;
+        }
+
+        Debug.Log("Killed all enemies");
+        
+        nextLevelIndex++;
+        string nextLevelName = "Level" + nextLevelIndex;
+
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
